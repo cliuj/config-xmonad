3dc6832730b2ad62df7ea5c8aa1c6a8824624f12
Charlie Liu <cliuj.dev@outlook.com>
2025-08-14 03:35:29 -0400

    feat: Add layout dependent configurations
    This basically adds "profiles" based on the layout used.
    Layouts now have independent keybindings and styling.

--- xmonad.hs
+++ xmonad.hs
@@ -34,6 +34,7 @@ import XMonad.Layout.BinarySpacePartition (emptyBSP, Rotate(Rotate), ResizeDirec
 import qualified XMonad.Layout.BinarySpacePartition as BSP
 import XMonad.Util.Types (Direction2D(L, R, U, D))
 import XMonad.Layout.WindowNavigation (windowNavigation, Navigate(..))
+import XMonad.Layout.Renamed (renamed, Rename(Replace))
 
 -- Data
 import Data.Monoid ()
@@ -77,8 +78,88 @@ myWorkspaces :: [String]
 myWorkspaces = [ "term", "code", "browser", "comm" ]
 
 -----------------------------------------------------------
+-- Layout-specific actions
+-----------------------------------------------------------
+
+-- Helper to get current layout description
+getLayoutDescription :: X String
+getLayoutDescription = do
+    ws <- gets windowset
+    return $ description . W.layout . W.workspace . W.current $ ws
+
+-- Execute action based on current layout
+layoutAction :: [(String, X ())] -> String -> X ()
+layoutAction actions key = do
+    layout <- getLayoutDescription
+    let action = case layout of
+            "BSP" -> lookup key bspActions
+            "Tall" -> lookup key tallActions
+            "Mirror Tall" -> lookup key mirrorTallActions
+            "Monocle" -> lookup key monocleActions
+            "Fullscreen" -> lookup key fullscreenActions
+            _ -> Nothing
+    maybe (return ()) id action
+
+-----------------------------------------------------------
 -- Keybindings
 -----------------------------------------------------------
+--
+-- BSP layout actions
+bspActions :: [(String, X ())]
+bspActions =
+    [ ("h", sendMessage $ Go L)
+    , ("j", sendMessage $ Go D)
+    , ("k", sendMessage $ Go U)
+    , ("l", sendMessage $ Go R)
+    , ("S-h", sendMessage $ Swap L)
+    , ("S-j", sendMessage $ Swap D)
+    , ("S-k", sendMessage $ Swap U)
+    , ("S-l", sendMessage $ Swap R)
+    , ("C-h", sendMessage $ ShrinkFrom R)
+    , ("C-j", sendMessage $ ExpandTowards D)
+    , ("C-k", sendMessage $ ShrinkFrom D)
+    , ("C-l", sendMessage $ ExpandTowards R)
+    , ("s", sendMessage BSP.Swap)
+    , ("r", sendMessage Rotate)
+    ]
+
+-- Tall layout actions
+tallActions :: [(String, X ())]
+tallActions =
+    [ ("j", windows W.focusDown)
+    , ("k", windows W.focusUp)
+    , ("S-j", windows W.swapDown)
+    , ("S-k", windows W.swapUp)
+    , ("h", sendMessage Shrink)
+    , ("l", sendMessage Expand)
+    ]
+
+-- Mirror Tall layout actions
+mirrorTallActions :: [(String, X ())]
+mirrorTallActions =
+    [ ("h", windows W.focusUp)
+    , ("l", windows W.focusDown)
+    , ("S-h", windows W.swapUp)
+    , ("S-l", windows W.swapDown)
+    , ("j", sendMessage Shrink)
+    , ("k", sendMessage Expand)
+    ]
+
+-- Monocle layout actions (Full with gaps)
+monocleActions :: [(String, X ())]
+monocleActions =
+    [ ("j", windows W.focusDown)
+    , ("k", windows W.focusUp)
+    ]
+
+-- Fullscreen layout actions (true fullscreen)
+fullscreenActions :: [(String, X ())]
+fullscreenActions =
+    [ ("j", windows W.focusDown)
+    , ("k", windows W.focusUp)
+    ]
+
+-- Layout independent keybindings
 myKeys :: [(String, X ())]
 myKeys =
     [   -- XMonad management
@@ -107,28 +188,21 @@ myKeys =
         ("M-.", nextScreen),
         ("M-S-,", sequence_ [shiftPrevScreen, prevScreen]),
         ("M-S-.", sequence_ [shiftNextScreen, nextScreen]),
-        -- Directional window focus (BSPWM-like, confined to current screen)
-        ("M-h", sendMessage $ Go L),
-        ("M-j", sendMessage $ Go D),
-        ("M-k", sendMessage $ Go U),
-        ("M-l", sendMessage $ Go R),
-        -- Directional window swapping
-        ("M-S-h", sendMessage $ Swap L),
-        ("M-S-j", sendMessage $ Swap D),
-        ("M-S-k", sendMessage $ Swap U),
-        ("M-S-l", sendMessage $ Swap R),
-        -- Traditional stack-based navigation (as fallback)
-        ("M-n", windows W.focusDown),
-        ("M-p", windows W.focusUp),
-        ("M-m", windows W.focusMaster),
-        ("M-S-m", windows W.swapMaster),
-        -- BinarySpacePartition controls
-        ("M-C-l", sendMessage $ ExpandTowards R),
-        ("M-C-h", sendMessage $ ShrinkFrom R),
-        ("M-C-j", sendMessage $ ExpandTowards D),
-        ("M-C-k", sendMessage $ ShrinkFrom D),
-        ("M-s", sendMessage BSP.Swap),
-        ("M-r", sendMessage Rotate),
+        -- Layout-aware navigation
+        ("M-h", layoutAction [] "h"),
+        ("M-j", layoutAction [] "j"),
+        ("M-k", layoutAction [] "k"),
+        ("M-l", layoutAction [] "l"),
+        ("M-S-h", layoutAction [] "S-h"),
+        ("M-S-j", layoutAction [] "S-j"),
+        ("M-S-k", layoutAction [] "S-k"),
+        ("M-S-l", layoutAction [] "S-l"),
+        ("M-C-l", layoutAction [] "C-l"),
+        ("M-C-h", layoutAction [] "C-h"),
+        ("M-C-j", layoutAction [] "C-j"),
+        ("M-C-k", layoutAction [] "C-k"),
+        ("M-s", layoutAction [] "s"),
+        ("M-r", layoutAction [] "r"),
         -- Applications
         ("M-b", spawn myBrowser)
     ]
@@ -142,13 +216,50 @@ myAdditionalKeys conf = let modm = modMask conf in M.fromList $
 -- Layout 
 -----------------------------------------------------------
 
+-- Helper for spacing
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border 0 i 0 i) True (Border i 0 i 0) True
 
-myLayoutHook = avoidStruts $ windowNavigation $ mySpacing 40 $
-             emptyBSP ||| tall ||| Mirror tall ||| Full
-                where 
-                    tall = Tall 1 (3/100) (1/2)
+-- Individual layout definitions with custom modifiers
+-- BSP with 40px spacing
+bspLayout = renamed [Replace "BSP"] 
+          $ avoidStruts 
+          $ windowNavigation 
+          $ mySpacing 40 
+          $ emptyBSP
+
+-- Tall with 30px spacing
+tallLayout = renamed [Replace "Tall"] 
+           $ avoidStruts 
+           $ windowNavigation 
+           $ mySpacing 30 
+           $ Tall 1 (3/100) (1/2)
+
+-- Mirror Tall with 30px spacing
+mirrorTallLayout = renamed [Replace "Mirror Tall"] 
+                 $ avoidStruts 
+                 $ windowNavigation 
+                 $ mySpacing 30 
+                 $ Mirror (Tall 1 (3/100) (1/2))
+
+-- Full (monocle) with spacing
+monocleLayout = renamed [Replace "Monocle"] 
+              $ avoidStruts 
+              $ windowNavigation 
+              $ mySpacing 40 
+              $ Full
+
+-- True fullscreen without any gaps or struts
+fullscreenLayout = renamed [Replace "Fullscreen"] 
+                 $ windowNavigation 
+                 $ Full
+
+-- Combined layout hook
+myLayoutHook = bspLayout 
+           ||| tallLayout 
+           ||| mirrorTallLayout 
+           ||| monocleLayout 
+           ||| fullscreenLayout
 
 -----------------------------------------------------------
 -- Main
